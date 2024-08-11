d4401178bf3c367de52acbbd39b2c436534edda0
Kei-gits <kioo-17@hotmail.com>
2024-08-11 21:34:00 +0200

    feat home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, Input, WritableSignal} from '@angular/core';
+import {FilterLocationComponent} from "../shared/components/filter-location/filter-location.component";
+import {ZipCity} from "../shared/models/address.model";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FilterLocationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  @Input() filters!: WritableSignal<Map<string, string>>;
+  @Input() page!: WritableSignal<number>;
+
+  addFilter(filter: ZipCity) {
+    this.filters.update((filters) => {
+      return new Map([...filters, ['city', filter.city], ['zip', filter.zip]]);
+    });
+
+    this.page.set(0);
+  }
+}
