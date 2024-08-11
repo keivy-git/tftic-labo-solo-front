9c6ad7a3d5dc9459b5145d70f4b0006c918b6cb8
Kei-gits <kioo-17@hotmail.com>
2024-08-11 21:28:50 +0200

    rename routes => AppRoute

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,21 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withDebugTracing } from '@angular/router';
 
-import { routes } from './app.route';
+import {AppRoute} from './app.route';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { MessageService } from 'primeng/api';
 import { baseInterceptor } from './core/interceptors/base.interceptor';
 import { jwtInterceptor } from './core/interceptors/jwt.interceptor';
+import {AuthService} from "./features/auth/services/auth.service";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withDebugTracing()),
+    provideRouter(AppRoute, withDebugTracing()),
     provideAnimationsAsync(),
     provideHttpClient(withInterceptors([baseInterceptor, jwtInterceptor])),
     MessageService,
+    AuthService
   ],
 };
