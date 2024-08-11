6a87ac7b017f5196f4a774cf51915445cec14b5c
Kei-gits <kioo-17@hotmail.com>
2024-08-11 21:34:22 +0200

    feat login

--- src/app/features/auth/pages/login/login.component.ts
+++ src/app/features/auth/pages/login/login.component.ts
@@ -1,17 +1,24 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { FormFieldComponent } from '../../../../shared/components/form-field/form-field.component';
 import { PasswordModule } from 'primeng/password';
 import { Button } from 'primeng/button';
 import { AuthService } from '../../services/auth.service';
 import { NotificationService } from '../../../../core/services/notification.service';
 import { Router } from '@angular/router';
 import { LOGIN_FORM } from '../../forms/login.form';
+import {FloatLabelModule} from "primeng/floatlabel";
+import {InputTextModule} from "primeng/inputtext";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, FormFieldComponent, PasswordModule, Button],
+  imports: [
+    ReactiveFormsModule,
+    PasswordModule,
+    Button,
+    FloatLabelModule,
+    InputTextModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
