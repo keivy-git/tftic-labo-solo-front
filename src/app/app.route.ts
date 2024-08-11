b1767679faa979b9a2620e95041a0816dc3916bf
Kei-gits <kioo-17@hotmail.com>
2024-08-11 21:29:06 +0200

    rename routes => AppRoute

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AppComponent } from './app.component';
 
-export const routes: Routes = [
+export const AppRoute: Routes = [
   {
     path: '',
     children: [
@@ -14,7 +12,7 @@ export const routes: Routes = [
       {
         path: '',
         loadChildren: () =>
-          import('./features/features.route').then((m) => m.routes),
+          import('./features/features.route').then((m) => m.FeaturesRoutes),
       },
     ],
   },
