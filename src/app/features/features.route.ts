af4f8a507690f20edac8e87041f911ba75437db3
Kei-gits <kioo-17@hotmail.com>
2024-08-11 21:32:04 +0200

    rename Route => FeatureRoutes Add fleamarket route and contact route

--- src/app/features/features.route.ts
+++ src/app/features/features.route.ts
@@ -1,13 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
+export const FeaturesRoutes: Routes = [
   {
     path: '',
     children: [
       {
         path: '',
-        loadChildren: () => import('./auth/auth.route').then((m) => m.routes),
+        loadChildren: () => import('./auth/auth.route').then((m) => m.AuthRoute),
       },
+      {
+        path: '',
+        loadChildren: () => import('./flea-market/fleamarket.route').then((m) => m.FleamarketRoute),
+      },
+      {
+        path: '',
+        loadChildren: () => import('./contact/contact.route').then((m) => m.ContactRoute),
+      }
     ],
   },
 ];
