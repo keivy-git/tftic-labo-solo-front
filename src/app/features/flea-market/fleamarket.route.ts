c1564114357094be9f359884bb099b9c07dc0092
Kevin Joeghmans <k.joeghmans@stag.technofuturtic.education>
2024-08-13 16:05:27 +0200

    add route

--- src/app/features/flea-market/fleamarket.route.ts
+++ src/app/features/flea-market/fleamarket.route.ts
@@ -1,9 +1,14 @@
-import {Routes} from "@angular/router";
-import {FleaMarketComponent} from "./flea-market.component";
+import { Routes } from '@angular/router';
+import { FleaMarketComponent } from './flea-market.component';
+import { AddFleamarketComponent } from './components/add-fleamarket/add-fleamarket.component';
 
 export const FleamarketRoute: Routes = [
-    {
-      path: 'fleamarkets',
-      component: FleaMarketComponent,
-    },
-  ];
+  {
+    path: 'fleamarkets',
+    component: FleaMarketComponent,
+  },
+  {
+    path: 'add-fleamarkets',
+    component: AddFleamarketComponent,
+  },
+];
