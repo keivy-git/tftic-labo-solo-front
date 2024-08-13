e9d1b979cecfbb52569b26c27d6e28a9c1073c4b
Kevin Joeghmans <k.joeghmans@stag.technofuturtic.education>
2024-08-13 16:04:48 +0200

    add getOnelocation

--- src/app/features/flea-market/services/flea-market.service.ts
+++ src/app/features/flea-market/services/flea-market.service.ts
@@ -1,56 +1,71 @@
-import {inject, Injectable} from "@angular/core";
-import {HttpClient, HttpParams} from "@angular/common/http";
-import {map, Observable, tap} from "rxjs";
-import {FleaMarket, PageFleaMarket} from "../models/flea-market.model";
-import {COMMON} from "../../../core/constants/common";
-import {API_ENDPOINTS} from "../../../core/constants/api-endpoint";
-import {ZipCity} from "../../../shared/models/address.model";
-import {FleaMarket_Form, FleaMarketForm} from "../forms/flea-market-form";
+import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { map, Observable, tap } from 'rxjs';
+import { FleaMarket, PageFleaMarket } from '../models/flea-market.model';
+import { COMMON } from '../../../core/constants/common';
+import { API_ENDPOINTS } from '../../../core/constants/api-endpoint';
+import { ZipCity } from '../../../shared/models/address.model';
+import { FleaMarketForm } from '../forms/flea-market-form';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FleaMarketService {
-  private readonly httpClient = inject(HttpClient)
+  private readonly httpClient = inject(HttpClient);
 
   getAllFleaMarket(
     params: Map<string, string> = new Map(),
     page: number = 0,
-  ):Observable<PageFleaMarket> {
-      let httpParams = new HttpParams().set(COMMON.page, page.toString());
+  ): Observable<PageFleaMarket> {
+    let httpParams = new HttpParams().set(COMMON.page, page.toString());
 
-      for (let [key, value] of params) {
-        httpParams = httpParams.append(key, value);
-      }
+    for (let [key, value] of params) {
+      httpParams = httpParams.append(key, value);
+    }
 
-      return this.httpClient
-        .get<PageFleaMarket>(`${API_ENDPOINTS.fleaMarket.fleaMarketGetAll}`, {
-          params: httpParams,
-        })
-        .pipe(
-          map((pageFleaMarket) => {
-            return {
-              ...pageFleaMarket,
-              fleaMarkets: pageFleaMarket.fleaMarkets.map(
-                (fleaMarket: FleaMarket) =>
-                  ({
-                    ...fleaMarket,
-                    createdAt: parseDate(fleaMarket.createdAt),
-                  }) as FleaMarket,
-              )
-            }
-          })
-        )
+    return this.httpClient
+      .get<PageFleaMarket>(`${API_ENDPOINTS.fleaMarket.fleaMarketGetAll}`, {
+        params: httpParams,
+      })
+      .pipe(
+        map((pageFleaMarket) => {
+          return {
+            ...pageFleaMarket,
+            fleaMarkets: pageFleaMarket.fleaMarkets.map(
+              (fleaMarket: FleaMarket) =>
+                ({
+                  ...fleaMarket,
+                  createdAt: parseDate(fleaMarket.createdAt),
+                }) as FleaMarket,
+            ),
+          };
+        }),
+      );
   }
 
   getAllLocations(): Observable<ZipCity[]> {
     return this.httpClient.get<ZipCity[]>(API_ENDPOINTS.zipCity.zipCity);
   }
 
-  postFleaMarket(newFleaMarket: FleaMarketForm): Observable<FleaMarketForm> {
-    return this.httpClient.post<FleaMarketForm>(API_ENDPOINTS.fleaMarket.fleaMarketCreate, newFleaMarket);
+  getOneLocation(zip: string | null, city: string | null) {
+    let params = new HttpParams();
+    if (zip != null) {
+      params = params.set('zip', zip);
+    }
+    if (city != null) {
+      params = params.set('city', city);
+    }
+    return this.httpClient.get<ZipCity[]>(API_ENDPOINTS.zipCity.zipCity, {
+      params,
+    });
   }
 
+  postFleaMarket(newFleaMarket: FleaMarketForm): Observable<FleaMarketForm> {
+    return this.httpClient.post<FleaMarketForm>(
+      API_ENDPOINTS.fleaMarket.fleaMarketCreate,
+      newFleaMarket,
+    );
+  }
 }
 export const parseDate = (date: number | string | Date): Date => {
   return new Date(date);
